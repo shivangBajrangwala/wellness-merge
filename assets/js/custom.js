602f17bdf54e13c99327d5e0602cbfb0fb6535dc
Shivang Bajrangwala <shivangb209@gmail.com>
2025-02-15 03:00:28 +0530

    home responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,3 +12,21 @@ jQuery(document).ready(function () {
     });
     // Sticky Header JS End
 })
+
+// Toggle Menu Mobile JS Start
+if (jQuery(window).width() < 992) {
+    jQuery(".ham-menu").on("click", function () {
+        jQuery(".header-menu").toggleClass("toggle-menu");
+        jQuery(".header-menu .nav-overlay").fadeToggle();
+    });
+    jQuery(".site-navigation ul li a").on("click", function () {
+        jQuery(".header-menu").removeClass("toggle-menu");
+        jQuery(".header-menu .nav-overlay").fadeOut();
+    });
+
+    jQuery(".header-menu .nav-overlay").on("click", function () {
+        jQuery(this).fadeOut();
+        jQuery(".header-menu").removeClass("toggle-menu");
+    });
+}
+// Toggle Menu Mobile JS End
